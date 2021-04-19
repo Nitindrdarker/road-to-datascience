653012cf003ef83562f4ff59aeb37d953f821724
Nitindrdarker <oct2001nitin@gmail.com>
2021-04-19 19:59:49 +0530

    day 3

---
+++ concept/gradient_descent.py
@@ -0,0 +1,20 @@
+import numpy as np
+def gradient_descent(x, y):
+    #start with some value of m_curr and b_curr
+    m_curr = b_curr = 0
+    iterations = 10000 #number of baby steps
+    n = len(x)
+    learningrate = 0.08
+    for i in range(iterations):
+        y_predicted = m_curr * x + b_curr #as y = m*x + b
+        cost  =(1/n) * sum((y - y_predicted)**2)#same as sum([val**2 for val in (y-y_predicted)])
+        md = -(2/n) * sum(x * (y - y_predicted))#m derevative
+        bd = -(2/n) * sum(y - y_predicted)#b deravitive
+        m_curr = m_curr - learningrate * md
+        b_curr = b_curr - learningrate * bd
+        print(f'm: {m_curr}, b: {b_curr}, iteration: {i}, cost {cost}')
+
+
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([5, 7, 9, 11, 13])
+gradient_descent(x, y)
